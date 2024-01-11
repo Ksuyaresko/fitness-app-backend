6322a691e2af6c2b6ea7464c179d3bec9fceefaf
User <user@MacBook-Pro-von-MCM-10703.local>
2024-01-11 02:08:12 +0100

    add swagger

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger/swagger.json");
 require("dotenv").config();
 
+const router = require('express').Router();
 const authRouter = require("./routes/api/auth");
-
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -12,6 +14,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/users", authRouter);
 
