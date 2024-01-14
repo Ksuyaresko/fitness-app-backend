d94a4087498ceaa66c7abf300044e0aa981322a9
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2024-01-14 19:48:42 +0200

    update diary-product

--- controller/diary/products/delProductInDiaryByDate.js
+++ controller/diary/products/delProductInDiaryByDate.js
@@ -1,3 +1,16 @@
-const delProductInDiaryByDate = async (req, res) => {};
+// controller для видалення продукту, що було спожито користувачем, із щоденника в обрану дату
+
+const { ProductDiary } = require("../../../models/productDiary");
+const { HttpError } = require("../../../utils");
+
+const delProductInDiaryByDate = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await ProductDiary.findOneAndDelete(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "The product has been removed from the diary" });
+};
 
 module.exports = delProductInDiaryByDate;
