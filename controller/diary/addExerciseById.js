41df20a53885d47bdb7dbcbbc52993248799562c
AntonZinchenko32 <zinchenko309@gmail.com>
2024-01-19 23:56:11 +0200

    validation error fix
    in addExercise.js

--- controller/diary/addExerciseById.js
+++ controller/diary/addExerciseById.js
@@ -1,13 +1,17 @@
 const { DiaryExercise } = require("../../models");
 const { HttpError } = require("../../utils");
 const { isMatch } = require("date-fns");
+const { isValidObjectId } = require("mongoose");
 
 const addExerciseById = async (req, res) => {
   const { _id: owner } = req.user;
   const { date, exercise_ID, calories, time } = req.body;
 
-  const result = isMatch(date, "dd/MM/yyyy");
-  if (!result) throw HttpError(400, "Incorrect date format");
+  if (!isValidObjectId(exercise_ID))
+    throw HttpError(400, `invalid id: ${exercise_ID}`);
+
+  if (!isMatch(date, "dd/MM/yyyy"))
+    throw HttpError(400, "Incorrect date format");
 
   const doneExercise = await DiaryExercise.create({
     ownerId: owner,
