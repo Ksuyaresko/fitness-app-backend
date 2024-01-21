70569f5d06f3360912d44eb622339f5667d4ddbb
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-01-21 19:32:59 +0100

    add checks

--- controller/products/getAllProducts.js
+++ controller/products/getAllProducts.js
@@ -1,10 +1,40 @@
 const { Product } = require("../../models");
+const { HttpError } = require("../../utils");
 
 const getAllProducts = async (req, res) => {
   const { page = 1, limit = 10, categories, search, type } = req.query;
-
   const bloodType = req.user.settings.blood;
   const skip = (page - 1) * limit;
+
+  const validPage = parseInt(req.query.page) || 1;
+  const validLimit = parseInt(req.query.limit) || 10;
+
+   if (isNaN(validPage) || isNaN(validLimit)) {
+     throw HttpError(400, "Page and limit must be numeric values");
+   }
+
+  if (validPage < 1 || validLimit < 1) {
+    throw HttpError(400, `Invalid page or limit value`);
+  }
+
+  const validBloodTypes = [1, 2, 3, 4];
+  if (!validBloodTypes.includes(bloodType)) {
+    throw HttpError(400, `invalid blood type`);
+  }
+
+  const lowercaseType = type ? type.toLowerCase() : null;
+  if (
+    lowercaseType &&
+    !["recommended", "notrecommended"].includes(lowercaseType)
+  ) {
+    throw HttpError(400, `Invalid type value`);
+  }
+
+  if (categories) {
+    if (typeof categories !== "string") {
+      throw HttpError(400, `Categories must be a string`);
+    }
+  }
   let query = {};
 
   if (categories) {
