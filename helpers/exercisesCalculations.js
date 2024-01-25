72598cb75fa924d54e5b53d2dc837cd877d15951
AntonZinchenko32 <98322005+AntonZinchenko32@users.noreply.github.com>
2024-01-25 17:10:57 +0200

    Diary exercise update (#79)
    * exercises fix
    
    * adds caloriesRemainingTotal to diary response
    
    adds caloriesRemainingTotal to full diary response
    
    * Update exercisesCalculations.js
    
    convert minutes in seconds

--- helpers/exercisesCalculations.js
+++ helpers/exercisesCalculations.js
@@ -2,13 +2,19 @@ const exercisesCalculations = (diaryEntryes, dailyActivity) => {
   const caloriesBurned = diaryEntryes.reduce((accumulator, currentExercise) => {
     return accumulator + currentExercise.calories;
   }, 0);
-  const timeTotal = diaryEntryes.reduce((accumulator, currentExercise) => {
-    return accumulator + currentExercise.time;
-  }, 0);
+  const timeTotalInSeconds = diaryEntryes.reduce(
+    (accumulator, currentExercise) => {
+      return accumulator + currentExercise.time;
+    },
+    0
+  );
+
+  let sportsRemaining = dailyActivity * 60 - timeTotalInSeconds;
+  sportsRemaining = Math.round(sportsRemaining / 60);
 
   return {
     caloriesBurned,
-    sportsRemaining: dailyActivity - timeTotal,
+    sportsRemaining,
   };
 };
 module.exports = exercisesCalculations;
